b3080f959ba186c0e7f49a3af77368570869b8e2
s27083 <115408411+s27083@users.noreply.github.com>
2023-05-20 23:57:06 +0200

    Update task2

--- zjazd5/src/Task2.java
+++ zjazd5/src/Task2.java
@@ -3,18 +3,21 @@ public class Task2 {
 
     Task2() {
         Scanner scanner = new Scanner(System.in);
-        Set<Integer> set1 = new HashSet<>();
-        Set<Integer> set2 = new HashSet<>();
+        Set<Integer> zbior1 = new HashSet<>();
+        Set<Integer> zbior2 = new HashSet<>();
         String[] input1 = scanner.nextLine().split(" ");
         String[] input2 = scanner.nextLine().split(" ");
         for (String s : input1) {
-            set1.add(Integer.parseInt(s));
+            zbior1.add(Integer.parseInt(s));
         }
         for (String s : input2) {
-            set2.add(Integer.parseInt(s));
+            zbior2.add(Integer.parseInt(s));
         }
-        Set<Integer> intersection = new HashSet<>(set1);
-        intersection.retainAll(set2);
-        System.out.println(intersection);
+        Set<Integer> czescWspolna = new HashSet<>(zbior1);
+        czescWspolna.retainAll(zbior2);
+        Set<Integer> wynik = new HashSet<>(zbior1);
+        wynik.addAll(zbior2);
+        wynik.removeAll(czescWspolna);
+        System.out.println(wynik);
     }
 }
